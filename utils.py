eff5363c97c2bf061a5f7170624522e82a42bb5e
Manas Mahale <36999652+Manas02@users.noreply.github.com>
2021-12-20 19:57:10 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,44 @@
+import random
+import numpy as np
+import torch
+from torch.nn import functional as F
+
+def seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def top_k_logits(logits, k):
+    v, ix = torch.topk(logits, k)
+    out = logits.clone()
+    out[out < v[:, [-1]]] = -float('Inf')
+    return out
+
+@torch.no_grad()
+def sample(model, x, steps, temperature=1.0, sample=False, top_k=None):
+
+    block_size = model.get_block_size()
+
+    model.eval()
+
+    for _ in range(steps):
+        # crop context if needed
+        x_cond = x if x.size(1) <= block_size else x[:, -block_size:] 
+        logits, _ = model(x_cond)
+        # pluck the logits at the final step and scale by temperature
+        logits = logits[:, -1, :] / temperature
+        # optionally crop probabilities to only the top k options
+        if top_k is not None:
+            logits = top_k_logits(logits, top_k)
+        # apply softmax to convert to probabilities
+        probs = F.softmax(logits, dim=-1)
+        # sample from the distribution or take the most likely
+        if sample:
+            ix = torch.multinomial(probs, num_samples=1)
+        else:
+            _, ix = torch.topk(probs, k=1, dim=-1)
+        # append to the sequence and continue
+        x = torch.cat((x, ix), dim=1)
+
+    return x
